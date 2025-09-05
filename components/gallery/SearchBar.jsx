41f6ec59d0c88c59d0ae59690f5223b66f6e843a
KenaiAngel <angel.sanchezroj@alumno.buap.mx>
2025-09-05 01:25:08 -0600

    Fix styles at SearchBar ---Avoid display search icon with ia filter active ---Avoid display dropdown component with ia filter active

--- components/gallery/SearchBar.jsx
+++ components/gallery/SearchBar.jsx
@@ -50,7 +50,7 @@ export default function SearchBar (){
 
     return (
         <div className="relative w-full mx-auto">
-            <div className= {` flex items-center justify-around bg-zinc-100 dark:bg-white h-16 pl-2  w-full ${openDropSearch ? 'rounded-tr-3xl rounded-tl-4xl':'rounded-4xl '}`}>
+            <div className= {` flex items-center justify-around bg-zinc-100 dark:bg-white h-16 pl-2  w-full ${(openDropSearch && !stateFilter.aiActive) ? 'rounded-tr-3xl rounded-tl-4xl':'rounded-4xl '}`}>
 {
   stateFilter.status !== "NO_ACTION" && (
     <span
@@ -110,7 +110,7 @@ export default function SearchBar (){
                 </div>
             </div>
             {
-                openDropSearch && 
+                (openDropSearch && !stateFilter.aiActive) && 
                 <DropDownSearchBar/>
             }
         </div>
